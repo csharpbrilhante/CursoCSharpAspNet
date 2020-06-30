f5e04075fce6a800358dee69611abc6a2ba38a9e
Daniel Junior <daniel.junior@engegraph.com.br>
2020-06-29 21:07:21 -0300

    adicionados ao projeto

---
+++ Estudos/Condicional.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estudos
+{
+    public class Condicional
+    {
+        bool _valorA = false;
+        bool _valorB = true;
+        bool _valorC = true;
+
+        public void TestarAnd()
+        {
+            Console.WriteLine($"{_valorA} E {_valorB} = {(_valorA && _valorB ? "Verdadeiro" : "falso")}");
+            if(_valorA && _valorB)
+            {
+                Console.WriteLine("Verdadeiro");
+            }
+            else
+            {
+                Console.WriteLine("Falso");
+            }
+        }
+
+        public void TestarOr()
+        {
+            //Console.WriteLine($"{_valorA} OU ({_valorB} E {_valorC})  = {(_valorA || (_valorB && _valorC) ? "Verdadeiro" : "falso")}");
+
+            if (!(! _valorA))
+            {
+                Console.WriteLine("Verdadeiro");
+            }
+            else
+            {
+                Console.WriteLine("Falso");
+            }
+        }
+
+        public void TestarWhile()
+        {
+            var do_work = true;
+            var numero = 0;
+            
+            while (do_work)
+            {
+                numero++;
+                Console.WriteLine($"Número é {numero}");
+
+                if (numero >= 10)
+                    do_work = false;
+            }
+        }
+
+
+
+
+        public void RetornarDescricaoEstadoCivil(EstadoCivil pMeuEstado)
+        {
+            var resultado = "";
+
+            switch (pMeuEstado)
+            {
+                case EstadoCivil.Solteiro: resultado = "Solteiro"; break;
+                case EstadoCivil.Casado: resultado = "Casado"; break;
+                case EstadoCivil.Divorciado: resultado = "Divorciado"; break;
+                case EstadoCivil.Viuvo: resultado = "Viúvo"; break;
+                default: resultado = "Não consta"; break;
+            }
+
+            Console.WriteLine(resultado);
+
+        }
+    }
+
+    public enum EstadoCivil
+    {
+        Solteiro = 0,
+        Casado = 1,
+        Divorciado = 2, 
+        Viuvo = 3
+    }
+}
---
+++ SistemaBancario/frmCadastroCorrentista.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace SistemaBancario
+{
+    partial class frmCadastroCorrentista
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmCadastroCorrentista
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(576, 329);
+            this.Name = "frmCadastroCorrentista";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cadastro de Correntista";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
