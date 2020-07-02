e65d3a203660cedb55a505b01c8f16da97efd34f
Daniel Junior <daniel.junior@engegraph.com.br>
2020-07-01 21:12:01 -0300

    Primeiro repository de correntistas e cadastro de correntistas

--- SistemaBancario/frmCadastroCorrentista.Designer.cs
+++ SistemaBancario/frmCadastroCorrentista.Designer.cs
@@ -28,20 +28,133 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.txtCpfCnpj = new System.Windows.Forms.MaskedTextBox();
+            this.gridCorrentistas = new System.Windows.Forms.DataGridView();
+            this.btnNovo = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.gridCorrentistas)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(359, 10);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Cpf ou Cnpj";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(27, 30);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(329, 20);
+            this.txtNome.TabIndex = 3;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(404, 60);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(129, 31);
+            this.btnSalvar.TabIndex = 5;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // txtCpfCnpj
+            // 
+            this.txtCpfCnpj.Location = new System.Drawing.Point(362, 30);
+            this.txtCpfCnpj.Mask = "999\\.999\\.999\\-99";
+            this.txtCpfCnpj.Name = "txtCpfCnpj";
+            this.txtCpfCnpj.Size = new System.Drawing.Size(171, 20);
+            this.txtCpfCnpj.TabIndex = 6;
+            // 
+            // gridCorrentistas
+            // 
+            this.gridCorrentistas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridCorrentistas.Location = new System.Drawing.Point(27, 106);
+            this.gridCorrentistas.Name = "gridCorrentistas";
+            this.gridCorrentistas.Size = new System.Drawing.Size(506, 241);
+            this.gridCorrentistas.TabIndex = 7;
+            // 
+            // btnNovo
+            // 
+            this.btnNovo.Enabled = false;
+            this.btnNovo.Location = new System.Drawing.Point(27, 60);
+            this.btnNovo.Name = "btnNovo";
+            this.btnNovo.Size = new System.Drawing.Size(129, 31);
+            this.btnNovo.TabIndex = 8;
+            this.btnNovo.Text = "Novo";
+            this.btnNovo.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(162, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 31);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Alterar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(297, 60);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 31);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Excluir";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // frmCadastroCorrentista
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(576, 329);
+            this.ClientSize = new System.Drawing.Size(553, 368);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnNovo);
+            this.Controls.Add(this.gridCorrentistas);
+            this.Controls.Add(this.txtCpfCnpj);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
             this.Name = "frmCadastroCorrentista";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Cadastro de Correntista";
+            this.Load += new System.EventHandler(this.frmCadastroCorrentista_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridCorrentistas)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.MaskedTextBox txtCpfCnpj;
+        private System.Windows.Forms.DataGridView gridCorrentistas;
+        private System.Windows.Forms.Button btnNovo;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- SistemaBancario/frmCadastroCorrentista.cs
+++ SistemaBancario/frmCadastroCorrentista.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SistemaBancario.Modelos;
+using SistemaBancario.Repositorio;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,43 @@ namespace SistemaBancario
 {
     public partial class frmCadastroCorrentista : Form
     {
+        //criei o nosso "Dataset" de correntistas
+        readonly CorrentistaRepository repositorio;
+        private BindingSource _datasource;
+
         public frmCadastroCorrentista()
         {
             InitializeComponent();
+            repositorio = CorrentistaRepository.ObterInstancia();
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            //criei uma nova instancia de corretista
+            var novoCorrentista = new Correntista();
+
+            //atribui os dados do novo correntista para a instancia do novo correntista
+            novoCorrentista.Nome = txtNome.Text;
+            novoCorrentista.CpfCnpj = txtCpfCnpj.Text;
+
+            //invoquei o metodo create do dataset
+            repositorio.Create(novoCorrentista);
+            _datasource.ResetBindings(true);
+
+        }
+
+        private void CarregarLista()
+        {
+            var correntistas = repositorio.Read();
+            _datasource = new BindingSource();
+            _datasource.DataSource = correntistas;
+            gridCorrentistas.DataSource = _datasource;
+
+        }
+
+        private void frmCadastroCorrentista_Load(object sender, EventArgs e)
+        {
+            CarregarLista();
         }
     }
 }
