2efc26d24080074cb6e67aa676c4744c9ef5d7a7
Daniel Junior <daniel.junior@engegraph.com.br>
2020-08-19 19:28:17 -0300

    alterações para iniciar projeto do caixa eletronico

--- SistemaBancario.CaixaEletronico/Program.cs
+++ SistemaBancario.CaixaEletronico/Program.cs
@@ -16,7 +16,7 @@ namespace SistemaBancario.CaixaEletronico
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmPrincipal());
+            Application.Run(new FormContainer());
         }
     }
 }
